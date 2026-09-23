5d63733ee2bee97ba7b18db6a82794969856d7d7
kik <jiangangben52@gmail.com>
2026-09-23 16:04:33 +0900

    Add official X and Instagram buttons, cache 20260923-5

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ try {
   // 通知機能なしで続行
 }
 
-const CACHE_NAME = "ritsumeikamen-20260923-4";
+const CACHE_NAME = "ritsumeikamen-20260923-5";
 
 self.addEventListener("install", () => {
   self.skipWaiting();
